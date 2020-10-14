80f4fd2f21e23b49fa6afb426e868c5427f2abf3
= <=>
2020-10-15 02:08:08 +0600

    Updates

---
+++ precache-manifest.586d0e5230ef48f35c1c15b92f94efab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca7b9da473e90e549a3327eade54f89a",
+    "url": "/musicplayer/index.html"
+  },
+  {
+    "revision": "f6b24c4503ed8eb3a16d",
+    "url": "/musicplayer/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d8b1424af249462a51c3",
+    "url": "/musicplayer/static/js/2.b46d3ba3.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/musicplayer/static/js/2.b46d3ba3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6b24c4503ed8eb3a16d",
+    "url": "/musicplayer/static/js/main.e55852ca.chunk.js"
+  },
+  {
+    "revision": "a8e64052a97e6a856ea6",
+    "url": "/musicplayer/static/js/runtime-main.13b63414.js"
+  },
+  {
+    "revision": "8fb2bc7a29a4264bdcdfb156b065fea6",
+    "url": "/musicplayer/static/media/google.8fb2bc7a.svg"
+  },
+  {
+    "revision": "2a54ea042c1dc1d17e64daeeccca791d",
+    "url": "/musicplayer/static/media/music-bg.2a54ea04.svg"
+  }
+]);
